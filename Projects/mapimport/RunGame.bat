817b8144e5f33f9141a59c3238d1ede03fb0acaa
memlocator <apakokos@gmail.com>
2021-11-26 23:29:18 +0100

    Structure rearranged for submodule setup

--- Projects/mapimport/RunGame.bat
+++ Projects/mapimport/RunGame.bat
@@ -1,3 +1,23 @@
 @echo off
 
-%cd%/../../GameEngine/Release/GameEngine.exe %cd%/main.lua
+setlocal EnableDelayedExpansion
+
+set directory=%cd%
+set checkDirectory=%cd%/HornetEngine
+
+echo %directory%
+
+:findpath
+if exist "%checkDirectory%" (goto :pathfound)
+
+    set directory2=%directory%/..
+    set directory=%directory2%
+    set checkDirectory=%directory2%/HornetEngine
+    echo checking %checkDirectory%
+    goto :findpath
+
+:pathfound
+
+echo found %checkDirectory%
+
+%checkDirectory%/GameEngine/Release/GameEngine.exe %cd%/main.lua
--- Projects/projectTemplate/RunGame.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-%cd%/../../GameEngine/Release/GameEngine.exe %cd%/main.lua
